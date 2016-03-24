f99360b0d9ea2a3176c3e687492d2f380659ecce
Tobias Pickel <tobias.pickel@uni-ulm.de>
2016-03-24 19:42:13 +0100

    init

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<h1>404 :/</h1>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+  </head>
+  <body>
+    <header>
+
+    </header>
+    <div id="view"></div>
+    <footer>
+
+    </footer>
+  </body>
+</html>
